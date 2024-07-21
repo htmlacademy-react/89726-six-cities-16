cb6193ac31d189185d52a2ecd3f4ea7a02ea84e2
Anton Rychkov <antrychkov@gmail.com>
2024-07-21 14:41:15 +0500

    3.1 Добавил компонент 404 страницы

---
+++ src/pages/not-found-screen/not-found-screen.tsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+
+function NotFoundScreen() : JSX.Element {
+  return (
+    <section className="not-found">
+      <h1 className="not-found__title">Page not found</h1>
+      <p className="not-found__text">
+        Сорян, страница не существует
+      </p>
+      <Link to="/" className="not-found__link">
+        На главную страницу;
+      </Link>
+    </section>
+  );
+}
+
+export default NotFoundScreen;
